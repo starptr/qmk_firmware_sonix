ecc4b78dc31c55269d47f3d68c691cfcb14fc8a1
Yuto Nishida <yuto@berkeley.edu>
2025-06-15 15:42:03 -0700

    Use existing formatting of keyboard

--- keyboards/keychron/k3/keymaps/starptr-upstream/keymap.c
+++ keyboards/keychron/k3/keymaps/starptr-upstream/keymap.c
@@ -116,9 +116,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_MSNP,    KC_DEL,     RGB_MOD  ,
       KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                KC_PGUP  ,
       KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                KC_PGDN  ,
-      KC_CAPS,    H_SFTA,     H_ALTS,     H_DIRD,     H_CTLF,       KC_G,       KC_H,     H_CTLJ,     H_DIRK,     H_ALTL,     H_SFTSCLN,    KC_QUOT,                KC_ENT,                 KC_HOME  ,
-      _______,                KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RPRN,    KC_UP,      KC_END   ,
-      KC_LBRC,    KC_LALT,    KC_LGUI,                                        KC_SPC,                                         KC_RGUI,     MO(FN),    KC_RBRC,    KC_LEFT,    KC_DOWN,    KC_RGHT
+      KC_CAPS,    H_SFTA,     H_ALTS,     H_DIRD,     H_CTLF,     KC_G,       KC_H,       H_CTLJ,     H_DIRK,     H_ALTL,     H_SFTSCLN,  KC_QUOT,                KC_ENT,                 KC_HOME  ,
+      _______,                KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RPRN,                KC_UP,      KC_END   ,
+      KC_LBRC,    KC_LALT,    KC_LGUI,                                        KC_SPC,                                         KC_RGUI,    MO(FN),     KC_RBRC,    KC_LEFT,    KC_DOWN,    KC_RGHT
   ),
   [NO_TAP_HOLD] = LAYOUT_75_ansi(
   /*  0           1           2           3           4           5           6           7           8           9           10          11          12          13          14          15       */
